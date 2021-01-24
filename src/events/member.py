8abf8376e8c7b858d2f34ab8633d200ac06982f8
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-23 16:01:45 -0800

    update avatar if it gets created in the member service

--- src/events/member.py
+++ src/events/member.py
@@ -1,12 +1,13 @@
 import logging
 
-from ..services import MemberService
+from ..services import MemberService, MemberMaterializedService
 
 
 class Member:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.member_service = MemberService()
+        self.member_materialized_service = MemberMaterializedService()
 
     def handle_event(self, key, data):
         # this means a user has been updated from invited to active and data from member service can be used to
@@ -16,3 +17,9 @@ class Member:
             if members.total:
                 self.member_service.apply(instance=members.items[0], user_uuid=data['user_uuid'], status='pending')
         # I need to handle avatar updates in member
+        elif key == 'avatar_created':
+            self.logger.info('avatar created')
+            members = self.member_materialized_service.find(member=data['member_uuid'])
+            if members.total:
+                member = members.items[0]
+                self.member_materialized_service.apply(instance=member, avatar=data['s3_filename'])

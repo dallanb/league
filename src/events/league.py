b656bd93d3ff63e8fc2b2726e8ad77fc3252b10c
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-27 12:48:44 -0800

    update status of a member according to whether or not they are the creator or not

--- src/events/league.py
+++ src/events/league.py
@@ -12,15 +12,8 @@ class League:
 
     def handle_event(self, key, data):
         self.logger.info('league event')
-        # this means a user has been updated from invited to active and data from league service can be used to
-        # populate associated entry in league model
         if key == 'member_created':
             self.logger.info('member created')
-            self.logger.info(data['user_uuid'])
-            self.logger.info(data['owner_uuid'])
-            # if the user is the owner of the league then set the status to active
-            if data['user_uuid'] == data['owner_uuid']:
-                _ = self.member_service.update(uuid=data['uuid'], status='active')
         elif key == 'member_pending' or key == 'member_active' or key == 'member_inactive':
             self.logger.info('member updated')
             status = key.split('member_')[1]

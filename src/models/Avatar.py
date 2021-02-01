d031dcfb5f8f38e190c875404c23b28e95630f7e
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-01 11:03:53 -0800

    test migrations

--- src/models/Avatar.py
+++ src/models/Avatar.py
@@ -4,6 +4,7 @@ from .. import db
 
 class Avatar(db.Model, BaseMixin):
     s3_filename = db.Column(db.String, nullable=False)
+    src = db.Column(db.String, nullable=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)

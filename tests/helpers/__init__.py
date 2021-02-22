b96b56740b8876545aff587c5ca2aa7f1c228f68
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-22 14:34:24 -0800

    fix docker compose test and add common unit tests

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,5 +1,6 @@
 from .fetch_member import fetch_member
 from .fetch_members import fetch_members
+from .generate_uuid import generate_uuid
 from .league_notification_create import league_notification_create
 from .league_notification_update import league_notification_update
 from .member_notification_create import member_notification_create

bbf44853774f20252985fcdbc1da407991d0aabe
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-22 17:19:24 -0800

    add some delay to league notification

--- tests/unit/decorators/test_league_notification.py
+++ tests/unit/decorators/test_league_notification.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from src import services
@@ -15,6 +17,7 @@ def test_league_notification_league_created(reset_db, kafka_conn_last_msg):
 
 def test_league_notification_league_inactive(kafka_conn_last_msg):
     _ = services.LeagueService().update(uuid=pytest.league.uuid, status='inactive')
+    time.sleep(0.5)
     msg = kafka_conn_last_msg('leagues')
     assert msg.key is not None
     assert msg.key == 'league_inactive'
